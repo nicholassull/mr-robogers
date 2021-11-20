b9761c375f909f46c503fac56ed5e11b4a431032
Nick Sullivan <nicholassull@gmail.com>
2021-11-20 14:56:36 -0800

    add constant declaration for value of #input-name

--- js/scripts.js
+++ js/scripts.js
@@ -29,7 +29,9 @@ function friendlyRobot (numberArray) {
 $(document).ready(function() {
   $("#input-form").submit(function(event) {
     const userNumber = $("#input-number").val();
+    const userName = $("input-name").val();
     $(".hidden").hide();
+    $
     $("#output-span").text(friendlyRobot(arrayMaker(userNumber)));
     $("#output-span").fadeIn()
 
